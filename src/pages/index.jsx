3fb337d57dcb65d886b417dd47497f2838521d11
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-26 16:15:58 +0630

    Navbar

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <div>
+        <Link to="/company/history">History</Link>
+      </div>
+    </Layout>
+  )
+}
